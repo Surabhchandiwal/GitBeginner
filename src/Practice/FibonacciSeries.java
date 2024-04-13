09b79269c7c46539ff0b4f9322e13dbd5c7e7253
unknown <surabhtesting@gmail.com>
2024-04-13 19:22:19 +0530

    second commit

--- src/Practice/FibonacciSeries.java
+++ src/Practice/FibonacciSeries.java
@@ -4,14 +4,14 @@ public class FibonacciSeries {
 
 	public static void main(String[] args) {
 		// generate fibonacci series--0 1  1 2 3 5 8 13
-		int n1=0 , n2=1 , sum ;
+		int n1=0 , n2=1 , add ;
 		System.out.println(n1 + " " + n2 +" ");
 		
 		for(int i=2;i<10;i++) {
-			sum = n1 + n2;
-			System.out.println(" "+ sum);
+			add = n1 + n2;
+			System.out.println(" "+ add);
 			n1=n2;
-			n2=sum;
+			n2=add;
 		}
 
 	}
