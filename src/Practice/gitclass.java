589b741343cc3753666e61a69e5c45a35911195b
unknown <surabhtesting@gmail.com>
2024-04-15 07:43:48 +0530

    My third commit

--- src/Practice/gitclass.java
+++ src/Practice/gitclass.java
@@ -5,6 +5,8 @@ public class gitclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("print git");
+		System.out.println("logged in..");
+		System.out.println("logged out");
 
 	}
 
